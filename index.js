ee4bc3b660a79ae182ad901b7a5da421f1f668f4
dennis090 <110439765+dennis090@users.noreply.github.com>
2025-04-25 09:32:52 -0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,264 @@
+const drugs = [
+  {
+    id: 1,
+    name: "Amoxicillin",
+    category: "Antibiotic",
+    dosageMg: 500,
+    isPrescriptionOnly: true,
+    stock: 120,
+    manufacturer: "Pfizer",
+  },
+
+  {
+    id: 2,
+    name: "Paracetamol",
+    category: "Analgesic",
+    dosageMg: 1000,
+    isPrescriptionOnly: false,
+    stock: 200,
+    manufacturer: "GSK",
+  },
+
+  {
+    id: 3,
+    name: "Ibuprofen",
+    category: "Analgesic",
+    dosageMg: 400,
+    isPrescriptionOnly: false,
+    stock: 150,
+    manufacturer: "Bayer",
+  },
+
+  {
+    id: 4,
+    name: "Chloroquine",
+    category: "Antimalarial",
+    dosageMg: 250,
+    isPrescriptionOnly: true,
+    stock: 80,
+    manufacturer: "Sanofi",
+  },
+
+  {
+    id: 5,
+    name: "Ciprofloxacin",
+    category: "Antibiotic",
+    dosageMg: 500,
+    isPrescriptionOnly: true,
+    stock: 70,
+    manufacturer: "Pfizer",
+  },
+
+  {
+    id: 6,
+    name: "Loratadine",
+    category: "Antihistamine",
+    dosageMg: 10,
+    isPrescriptionOnly: false,
+    stock: 160,
+    manufacturer: "Novartis",
+  },
+
+  {
+    id: 7,
+    name: "Metformin",
+    category: "Antidiabetic",
+    dosageMg: 850,
+    isPrescriptionOnly: true,
+    stock: 140,
+    manufacturer: "Teva",
+  },
+
+  {
+    id: 8,
+    name: "Artemether",
+    category: "Antimalarial",
+    dosageMg: 20,
+    isPrescriptionOnly: true,
+    stock: 60,
+    manufacturer: "Roche",
+  },
+
+  {
+    id: 9,
+    name: "Aspirin",
+    category: "Analgesic",
+    dosageMg: 300,
+    isPrescriptionOnly: false,
+    stock: 180,
+    manufacturer: "Bayer",
+  },
+
+  {
+    id: 10,
+    name: "Omeprazole",
+    category: "Antacid",
+    dosageMg: 20,
+    isPrescriptionOnly: true,
+    stock: 90,
+    manufacturer: "AstraZeneca",
+  },
+
+  {
+    id: 11,
+    name: "Azithromycin",
+    category: "Antibiotic",
+    dosageMg: 250,
+    isPrescriptionOnly: true,
+    stock: 50,
+    manufacturer: "Pfizer",
+  },
+
+  {
+    id: 12,
+    name: "Cetirizine",
+    category: "Antihistamine",
+    dosageMg: 10,
+    isPrescriptionOnly: false,
+    stock: 110,
+    manufacturer: "Novartis",
+  },
+
+  {
+    id: 13,
+    name: "Insulin",
+    category: "Antidiabetic",
+    dosageMg: 100,
+    isPrescriptionOnly: true,
+    stock: 30,
+    manufacturer: "Novo Nordisk",
+  },
+
+  {
+    id: 14,
+    name: "Artemisinin",
+    category: "Antimalarial",
+    dosageMg: 100,
+    isPrescriptionOnly: true,
+    stock: 50,
+    manufacturer: "GSK",
+  },
+
+  {
+    id: 15,
+    name: "Codeine",
+    category: "Analgesic",
+    dosageMg: 30,
+    isPrescriptionOnly: true,
+    stock: 20,
+    manufacturer: "Teva",
+  },
+
+  {
+    id: 16,
+    name: "Vitamin C",
+    category: "Supplement",
+    dosageMg: 500,
+    isPrescriptionOnly: false,
+    stock: 300,
+    manufacturer: "Nature’s Bounty",
+  },
+
+  {
+    id: 17,
+    name: "Ranitidine",
+    category: "Antacid",
+    dosageMg: 150,
+    isPrescriptionOnly: false,
+    stock: 90,
+    manufacturer: "Sanofi",
+  },
+
+  {
+    id: 18,
+    name: "Doxycycline",
+    category: "Antibiotic",
+    dosageMg: 100,
+    isPrescriptionOnly: true,
+    stock: 40,
+    manufacturer: "Pfizer",
+  },
+
+  {
+    id: 19,
+    name: "Tramadol",
+    category: "Analgesic",
+    dosageMg: 50,
+    isPrescriptionOnly: true,
+    stock: 45,
+    manufacturer: "Teva",
+  },
+
+  {
+    id: 20,
+    name: "Folic Acid",
+    category: "Supplement",
+    dosageMg: 5,
+    isPrescriptionOnly: false,
+    stock: 250,
+    manufacturer: "Nature’s Bounty",
+  },
+];
+//question1
+const filteringAntibiotic = drugs.filter((drugs) => {
+  return drugs.category == "Antibiotic";
+});
+console.log(filteringAntibiotic);
+//question2
+const lowerCaseNameOfDrugs = drugs.map((drugs) => {
+  return drugs.name.toLocaleLowerCase();
+});
+console.log(lowerCaseNameOfDrugs);
+//question3
+function getDrugsByCategory(category) {
+  return drugs.filter((drug) => {
+    return drug.category.toLocaleLowerCase() === category.toLocaleLowerCase();
+  });
+}
+//question4
+const manufacutrerInfo = drugs.map((drugs) => {
+  return {
+    fname: drugs.name,
+    manufacturer: drugs.manufacturer,
+  };
+});
+console.log(manufacutrerInfo);
+//question 5
+const prescriptionRequired = drugs.filter((drugs) => {
+  return drugs.isPrescriptionOnly == true;
+});
+console.log(prescriptionRequired);
+//question6
+const newArray = drugs.map((drugs) => {
+  return `Drug: ${drugs.name} dosageMg ${drugs.dosageMg}`;
+});
+console.log(newArray);
+//question7
+const drugsStockLessThan50 = drugs.filter((drugs) => {
+  return drugs.stock < 50;
+});
+console.log(drugsStockLessThan50);
+//question 8
+const prescriptionNotRequired = drugs.filter((drugs) => {
+  if (drugs.isPrescriptionOnly != true) {
+    return drugs.isPrescriptionOnly == false;
+  }
+});
+console.log(prescriptionNotRequired);
+
+//question 9
+
+const company = drugs.manufacturer;
+const companyName = drugs.filter((drugs) => {
+  if (drugs.manufacturer === company) {
+    return drugs.manufacturer;
+  }
+});
+console.log(companyName.length);
+//question 10
+const availableDrugs = "Analgesic";
+let count = 0;
+drugs.forEach(() => {
+  if (drugs.manufacturer === availableDrugs) count++;
+});
+console.log(`${availableDrugs} appears ${count} times`);
